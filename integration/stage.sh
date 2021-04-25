ec7bc3480c4f6e8da3c4137df44213ba6b666c63
alexx19 <alexx198906@gmail.com>
2021-04-25 15:40:03 -0500

    set region

--- integration/stage.sh
+++ integration/stage.sh
@@ -12,7 +12,7 @@ configure_aws_cli() {
     aws --version
     aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
     aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
-    aws configure set defaul.region ${AWS_REGION}
+    aws configure set region ${AWS_REGION}
     aws configure set defaul.output json
 }
 
