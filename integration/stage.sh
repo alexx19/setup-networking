03343d9301edabd88c122ac80439420308bd0c76
alexx19 <alexx198906@gmail.com>
2021-04-25 19:57:07 -0500

    test deploy un

--- integration/stage.sh
+++ integration/stage.sh
@@ -1,7 +1,6 @@
 # archivo de configuracion despliegue en stage
-#!/bin/bash
-
-JQ="jq --raw-output --exit-status"
+#!/usr/bin/env bash
+set -e
 
 export AWS_REGION=us-east-1
 export ENTERPRISE_NAME=rex
@@ -10,25 +9,18 @@ export ENVIRONMENT=stage
 
 export AWS_INFRA_CLOUDFORMATION_STACK_NAME=${ENTERPRISE_NAME}-${ENVIRONMENT}-${APPLICATION_NAME}
 
-configure_aws_cli() {
-    echo -e "configure AWS CLI"
-    aws --version
-    aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
-    aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
-    aws configure set defaul.region ${AWS_REGION}
-    aws configure set defaul.output json
-
-    echo  ${AWS_INFRA_CLOUDFORMATION_STACK_NAME}
-}
-
-deploy_project() {
-    aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE UPDATE_COMPLETE UPDATE_ROLLBACK_COMPLETE
-    echo "Creating stack"
-    aws cloudformation create-stack --stack-name ${AWS_INFRA_CLOUDFORMATION_STACK_NAME} --template-body file://setup-networking.yml
-    echo "Finishing Deploying"
-}
-
 echo "INFO ------> CONFIGURE AWS CLI... "
-configure_aws_cli
+
+echo -e "configure AWS CLI"
+aws --version
+aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
+aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+aws configure set defaul.region ${AWS_REGION}
+aws configure set defaul.output json
+
 echo "INFO ------> DEPLOYING NETWORKING... "
-deploy_project
+
+aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE UPDATE_COMPLETE UPDATE_ROLLBACK_COMPLETE
+echo "Creating stack"
+aws cloudformation create-stack --stack-name ${AWS_INFRA_CLOUDFORMATION_STACK_NAME} --template-body file://setup-networking.yml
+echo "Finishing Deploying"
