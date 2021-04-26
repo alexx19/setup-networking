d23fe874664a67c8daf159d871f908afc94df912
alexx19 <alexx198906@gmail.com>
2021-04-25 21:20:56 -0500

    deploy ahora si stage

--- integration/stage.sh
+++ integration/stage.sh
@@ -14,7 +14,7 @@ configure_aws_cli() {
     aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
     aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
     aws configure set defaul.region ${AWS_REGION}
-    aws configure --profile stage
+    aws configure set profile stage
     aws configure set defaul.output json
     aws configure list
 }
