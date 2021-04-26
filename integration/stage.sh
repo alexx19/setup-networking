d05500bd8a08dbec4d3708d5c591366eec0813eb
alexx19 <alexx198906@gmail.com>
2021-04-25 20:35:22 -0500

    file config deploy

--- integration/stage.sh
+++ integration/stage.sh
@@ -20,7 +20,8 @@ configure_aws_cli() {
 
 deploy_project() {
     echo "Creating stack"
-    aws cloudformation create-stack --stack-name ${AWS_INFRA_CLOUDFORMATION_STACK_NAME} --template-body file://setup-networking.yml --capabilities CAPABILITY_NAMED_AIM --parameters $PARAMETERS
+    echo ${AWS_INFRA_CLOUDFORMATION_STACK_NAME}
+    aws cloudformation create-stack --stack-name ${AWS_INFRA_CLOUDFORMATION_STACK_NAME} --template-body file://setup-networking.yml
     echo "Finishing Deploying"
 }
 
